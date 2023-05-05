c04e860c5875e60bf2a3f5d3f79ffca9923a9815
Simon Austen <hello@simonausten.me>
2023-05-05 10:53:38 +0100

    Streamlit experiments

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+# message("Welcome to Ruby")
+# message("User message", is_user=True)
+# message("Lorem ipsum dolor sit amet")
+# message("consectetur adipiscing elit", is_user=True)
+
+# # # if 'message_history' not in st.session_state:
+# st.session_state['message_history'] = []
+
+# # for message_ in st.session_state['message_history']:
+# #     message(message_, is_user=True) # display all the previous messages
+
+# st.text_input('')
